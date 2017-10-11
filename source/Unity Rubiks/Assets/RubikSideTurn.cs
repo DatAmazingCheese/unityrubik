5f733543911a616255dcc71d4e97326de626e7eb
DatAmazingCheese <ryanh.personal@gmail.com>
2017-10-11 19:20:15 +1100

    Menu, folder change

---
+++ source/Unity Rubiks/Assets/RubikSideTurn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RubikSideTurn : MonoBehaviour
+{
+    public float RotateAngle = 90f;
+
+    IEnumerator RotateAround(Vector3 axis, float angle, float duration)
+    {
+        float elasped = 0f;
+        float rotated = 0f;
+
+        while (elasped < duration)
+        {
+            float step = angle / duration * Time.deltaTime;
+            transform.RotateAround(transform.position, axis, step);
+            elasped += Time.deltaTime;
+            rotated += step;
+            yield return null;
+        }
+
+        transform.RotateAround(transform.position, axis, angle - rotated);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(RotateAround(Vector3.up, RotateAngle, 1.0f));
+        }
+    }
+}
